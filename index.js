78505a80561a35ae35e17f09e00ddbebd4b8df8f
coodingalex <coodingalex@gmail.com>
2020-06-24 19:24:18 -0600

    fixing now

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const portfolioRouter = require('./routes/portfolio')
 app.use(express.json())
 app.use(cors())
 
-app.get('/', (req, res) => {
+app.get('/hola', (req, res) => {
   res.json({ hello: 'portfolio' })
 })
 
