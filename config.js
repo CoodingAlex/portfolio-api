21f533cbead9bc647f6454c04ad411e870bc8ee5
coodingalex <coodingalex@gmail.com>
2020-07-01 00:46:09 -0600

    emails routes

--- config.js
+++ config.js
@@ -11,4 +11,14 @@ module.exports = {
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
   },
+  nodemailer:{
+    auth:{
+      type: 'oauth2',
+      user: process.env.GMAIL_ADRESS,
+      clientId: process.env.GMAIL_CLIENT_ID,
+      clientSecret: process.env.GMAIL_CLIENT_SECRET,
+      refreshToken: process.env.GMAIL_REFRESH_TOKEN,
+      accessToken: process.env.GMAIL_ACCESS_TOKEN
+    }
+  }
 }
--- index.js
+++ index.js
@@ -3,16 +3,22 @@ const debug = require('debug')('portfolio:index')
 const app = express()
 const port = process.env.PORT || 3000
 const cors = require('cors')
+const config = require('./config')
 //routes
+const emailsRouter = require('./routes/email')
 const portfolioRouter = require('./routes/portfolio')
 app.use(express.json())
+app.use(express.urlencoded())
 app.use(cors())
+
+
 
 app.get('/', (req, res) => {
   res.json({ hello: 'portfolio' })
 })
 
 app.use('/portfolio', portfolioRouter)
+app.use('/emails',emailsRouter)
 app.listen(port, () => {
   console.log(`Api listening on port ${port}`)
 })
