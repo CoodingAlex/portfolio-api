01ad68a348af3422699d0f10634660bce9eed087
coodingalex <coodingalex@gmail.com>
2020-07-01 02:28:09 -0600

    why it works on heroku but on zeit dont?

--- routes/portfolio.js
+++ routes/portfolio.js
@@ -54,17 +54,16 @@ router.post('/send', (req, res, next) => {
     let transporter = nodemailer.createTransport({
       service: 'gmail',
       auth: {
-        // type: 'OAuth2',
-        // user: config.nodemailer.auth.user,
-        // clientId: config.nodemailer.auth.clientId,
-        // clientSecret: config.nodemailer.auth.clientSecret,
-        // accessToken: config.nodemailer.auth.accessToken,
-        // refreshToken: config.nodemailer.auth.refreshToken,
-        user: process.env.GMAIL_ACCOUNT,
-        pass: process.env.GMAIL_PASS
+        type: 'OAuth2',
+        user: config.nodemailer.auth.user,
+        clientId: config.nodemailer.auth.clientId,
+        clientSecret: config.nodemailer.auth.clientSecret,
+        accessToken: config.nodemailer.auth.accessToken,
+        refreshToken: config.nodemailer.auth.refreshToken,
+        // user: process.env.GMAIL_ACCOUNT,
+        // pass: process.env.GMAIL_PASS
       },
     })
-    console.log(process.env.GMAIL_ACCOUNT,process.env.GMAIL_PASS);
     
     transporter.sendMail(mailOptions, (err, res) => {
       if (err) {
